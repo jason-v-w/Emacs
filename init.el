ecd5e6e8523fc6c577716fc49744d7d9c0ef765a
Jason Vander Woude <jasonvwoude@gmail.com>
2017-08-02 23:10:21 -0500

    Periodic commit

--- init.el
+++ init.el
@@ -136,13 +136,14 @@
   
   nil)
 
-(load-theme `wombat)
+;; (load-theme `wombat)
+(load-theme `adwaita)
 ;;(menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (setq frame-title-format "Emacs")
 (setq inhibit-splash-screen t)
-(transparency 85)
+(transparency 95)
 
 (add-to-list 'org-structure-template-alist
              '("m" "#+BEGIN_SRC emacs-lisp\n?\n#+END_SRC" "<src lang=\"?\">\n\n</src>"))
@@ -243,6 +244,27 @@
 (define-key origami-mode-map (kbd "C-c j") 'origami-forward-toggle-node)
 (global-origami-mode 1)
 
+(require 'rainbow-delimiters)
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'ess-mode-hook 'rainbow-delimiters-mode)
+
+(require 'rainbow-identifiers)
+;; (add-hook 'prog-mode-hook 'rainbow-identifiers-mode)
+;; (add-hook 'ess-mode-hook 'rainbow-identifiers-mode)
+
+(require 'projectile)
+(add-hook 'prog-mode-hook 'projectile-mode)
+(add-hook 'ess-mode-hook 'projectile-mode)
+
+(setq helm-projectile-fuzzy-match t)
+(require 'helm-projectile)
+(helm-projectile-on)
+
+;; (require 'powerline)
+;; (powerline-default-theme)
+
+(require 'git-timemachine)
+
 (load "~/.emacs-Macaulay2" t)
 (global-set-key (kbd "C-c C-l") 'M2-send-to-program)
 
@@ -297,6 +319,76 @@
 (global-set-key (kbd "<kp-add>") 'evil-numbers/inc-at-pt)
 (global-set-key (kbd "<kp-subtract>") 'evil-numbers/dec-at-pt)
 
+(require 'helm)
+    (require 'helm-config)
+
+    ;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+    ;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+    ;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+    (global-set-key (kbd "C-c h") 'helm-command-prefix)
+    (global-unset-key (kbd "C-x c"))
+
+    (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+    (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
+    (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+    (when (executable-find "curl")
+      (setq helm-google-suggest-use-curl-p t))
+
+    (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+          helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+          helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+          helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+          helm-ff-file-name-history-use-recentf t
+          helm-echo-input-in-header-line t)
+
+    (defun spacemacs//helm-hide-minibuffer-maybe ()
+      "Hide minibuffer in Helm session if we use the header line as input field."
+      (when (with-helm-buffer helm-echo-input-in-header-line)
+        (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+          (overlay-put ov 'window (selected-window))
+          (overlay-put ov 'face
+                       (let ((bg-color (face-background 'default nil)))
+                         `(:background ,bg-color :foreground ,bg-color)))
+          (setq-local cursor-type nil))))
+
+
+    (add-hook 'helm-minibuffer-set-up-hook
+              'spacemacs//helm-hide-minibuffer-maybe)
+
+    ;; Allow helm to automatically size the buffer to fit the content
+    (helm-autoresize-mode t)
+    ;; Numbers are percentages but I don't understand the 0
+    (setq helm-autoresize-max-height 0)
+    (setq helm-autoresize-min-height 40)
+
+
+    (helm-mode 1)
+
+  ;; Use helm-M-x instead of M-x
+  (global-set-key (kbd "M-x") 'helm-M-x)
+  (setq helm-M-x-fuzzy-match t) ;; optional fuzzy matching for helm-M-x
+
+  (global-set-key (kbd "C-x b") 'helm-mini)
+  (setq helm-buffers-fuzzy-matching t
+      helm-recentf-fuzzy-match t)
+
+  (global-set-key (kbd "M-y") 'helm-show-kill-ring)
+
+  (require 'helm-swoop)
+  (global-set-key (kbd "C-S-s") 'helm-swoop)
+
+  (require 'ace-jump-helm-line)
+  (eval-after-load "helm"
+    '(define-key helm-map (kbd "M-;") 'ace-jump-helm-line))
+
+;; Code from [[https://www.reddit.com/r/emacs/comments/345vtl/make_helm_window_at_the_bottom_without_using_any/][emacs reddit]]
+(add-to-list 'display-buffer-alist
+                    `(,(rx bos "*helm" (* not-newline) "*" eos)
+                         (display-buffer-in-side-window)
+                         (inhibit-same-window . t)
+                         (window-height . 0.4)))
+
 (global-set-key (kbd "C-x b") 'helm-buffers-list)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 
@@ -307,6 +399,11 @@
 (define-key global-map (kbd "<S-iso-lefttab>") 'origami-forward-toggle-node)
 
 (global-set-key (kbd "C-S-l") 'my-horizontal-recenter)
+
+;; This should be updated to allow for things such as 
+;;   M-m f for find files
+;;   M-m b for switch buffers
+(global-set-key (kbd "M-m") (lookup-key global-map (kbd "C-x")))
 
 (if (and (fboundp 'server-running-p) 
          (not (server-running-p)))
